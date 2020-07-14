83c36d59c3658bca223e6751e1297e5fd11e9785
Thomas Schneider <snieda@web.de>
2020-07-14 20:45:54 +0200

    colored

---
+++ menu/menu-alias.sh
@@ -0,0 +1,26 @@
+# convenience methods for styling
+
+RESTORE='\x1b[0m'
+R=$RESTORE
+
+RED='\x1b[00;31m'
+GREEN='\x1b[00;32m'
+YELLOW='\x1b[00;33m'
+BLUE='\x1b[00;34m'
+PURPLE='\x1b[00;35m'
+CYAN='\x1b[00;36m'
+LIGHTGRAY='\x1b[00;37m'
+
+LRED='\x1b[01;31m'
+LGREEN='\x1b[01;32m'
+LYELLOW='\x1b[01;33m'
+LBLUE='\x1b[01;34m'
+LPURPLE='\x1b[01;35m'
+LCYAN='\x1b[01;36m'
+WHITE='\x1b[01;37m'
+
+C_FRM=$BLUE
+C_CMD=$LCYAN
+C_PAR=$LGREEN
+C_CMT=$LRED
+
--- menu/menu.sh
+++ menu/menu.sh
@@ -2,11 +2,13 @@
 ##############################################################################
 # provides a simple menu (cp Thomas Schneider 2020)
 #
-# usage: menu <name> [file-name or wildcard] [-s]
+# usage: menu <name> [file-name or wildcard] [-s] [subst=true] [banner=<banner>]
 #
 # - name        : will be used as variable name and to read the file '$1.lst'
 # - file-or-glob: if wildcard, select from file-list, else real file name
 # - -s          : simple list separated by spaces
+# - subst=true  : variables of your menu list file will be substituted
+# - banner=<banner>: use <banner> instead of figlet
 #
 # - each line has structure: COMMAND # DESCRIPTION
 #   with:
@@ -23,7 +25,6 @@
 #    - if figlet is installed, an ascii title will be printed
 #    - if the file 'menu-alias.sh' was found it will be loaded
 #    - finally, the file 'menu-$USERNAME.sh' with results will be created
-#    - if subst=true the menu list variables will be substituted
 ##############################################################################
 
 BANNER=<<EOF
@@ -35,7 +36,7 @@ BANNER=<<EOF
                          
 EOF
 
-[ "$1" == "" ] && head -n 27 $0 && exit 1
+[ "$1" == "" ] && head -n 28 $0 && exit 1
 for a in "$*" ; do declare -gt $a; echo $a; done;
 
 [ -e menu-alias.sh ] && source menu-alias.sh
@@ -46,17 +47,22 @@ run() { source $*; }
 edit() { read -ep "$1: " -i $1 edit; declare -xt $1=$edit; NAME=$1; CMD=$edit; } 
 choose() { source menu.sh $1 "$2" "$3"; }
 bar__() { for i in {1..26}; do printf "%s" $1; done; }
-printbar() { printf "<"; bar__ "-"; printf "|%14s%-14s|" $1 $2; bar__ "-"; printf ">%s" $'\n'; }
+printbar() { printf "$C_FRM<"; bar__ "-"; printf "|%14s%-14s|" $1 $2; bar__ "-"; printf ">%s$R" $'\n'; }
+setcolors() { echo "$1" | sed -nEe "s/(\w+)(.*)([#].*)/$C_CMD\1$C_PAR\2$C_CMT\3$R/p"; }
 
 NAME=$1
 case $2 in *[*]*) LIST=$2;; *[.]*) LIST=$(< $2);; *) LIST=$(< $NAME.lst) ;; esac
-[ "$subst" == "true" ] && LIST=$(printf "$LIST" | envsubst)
+[ "$subst" == "true" ] && LIST=$(printf "$LIST" | envsubst "$(set -o posix; set)")
+LIST=$(setcolors "$LIST")
 IFS_=$IFS
 [ "$2" != "-s" ] && [ "$3" != "-s" ] && IFS=$'\n'
 PS3="$(printbar "PLEASE SELECT" " 1..$(echo "$LIST" | wc -l)") "$'\n'": "
 
-clear
-if [ "$(which figlet)" != "" ]; then figlet "$NAME"; else head -n 34 $0 | tail -n 6; fi
+banner=${banner:-figlet}
+ascii_banner_installed=$(which $banner) # on cygwin, 'which' prints always the path
+#clear
+echo -en $LYELLOW
+if [ "$ascii_banner_installed" != "" ]; then $banner "$NAME"; else head -n 36 $0 | tail -n 6; fi
 printbar "PLEASE SELECT" " $NAME"
 select i in $LIST ; do 
 	[ "$i" == "" ] || [ "$i" == "0" ] && break
