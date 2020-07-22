a24da4a4703d12e6888c1040aec5bef504e2d905
Thomas Schneider <snieda@web.de>
2020-07-22 23:10:55 +0200

    fixes

--- menu/menu.sh
+++ menu/menu.sh
@@ -3,16 +3,15 @@
 # provides a simple menu (cp Thomas Schneider 2020)
 #
 # usage: menu <name> [file-name or wildcard] [-s] [subst=true] [BANNER=<banner>]
-#             [EDITOR=<editor>] [reset=<clear>] [export=true]
+#             [EDITOR=<editor>] [before=<clear>] [log=true]
 # - name        : will be used as variable name and to read the file '$1.lst'
 # - file-or-glob: if wildcard, select from file-list, else menu file name
 # - -s          : simple list separated by spaces
 # - subst=true  : variables of your menu list file will be substituted
 # - BANNER=<banner>: use <banner> instead of figlet
 # - EDITOR=<editor>: use <editor> instead of vim
-# - reset=<clear>  : use output instead of clear
-# - export=true    : the content of menu-defaults.def and menu-$USERNAME.def
-#                    will be exported (may cause problems...)
+# - before=<clear> : (default:clear) do something between init and menu start
+# - log=true       : redirects 'echo' to 'printf | tee log'
 #
 # - each line has structure: COMMAND # DESCRIPTION
 #   with:
@@ -42,18 +41,22 @@ title=<<EOF
                          
 EOF
 
-[ "$1" == "" ] && head -n 34 $0 && exit 1
-for a in "$*" ; do [ "$a" == *"="* ] && declare -gt $a; echo $a; done;
+echo "starting: $0 $@"
+[ "$1" == "" ] || [ "$1" == "--help" ] && head -n 33 $0 && exit 1
+echo -en "setting parameters: "; for a in "$*" ; do [[ "$a" == *"="* ]] && declare -gt $a && echo $a; done;
 
 [ -e menu-alias.sh ] && source menu-alias.sh
-[ -e menu-defaults.def ] && [ "$export" == "true" ] && (export $(xargs <menu-defaults.def) || source menu-defaults.def)
-[ -e menu-$USERNAME.def ] && [ "$export" == "true" ] && (export $(xargs <menu-$USERNAME.def) || source menu-$USERNAME.def)
+[ -e menu-defaults.def ] && source menu-defaults.def
+[ -e menu-$USERNAME.def ] && source menu-$USERNAME.def
 
+[ "$log" == "true" ] && echo() { printf "%s\n" $* | tee -a menu.log; }
 run() { bash $1 $2 $3; }
-input() { read -ep ": " -i "${!1}" edt; declare -g $1=$edt; echo "$1=$edt" >> menu-$USERNAME.def; } 
+input() { read -ep ": " -i "${!1}" edt; declare -g $1=$edt; echo "$1=\"$edt\"" >> menu-$USERNAME.def; } 
 choose() { bash menu.sh "$@"; }
 edit() { ${EDITOR:-vim} "$*"; }
 show() { echo "$@"; }
+sysinfo() { uname -a; id; df -TPh; ifconfig; ipconfig; cat /proc/cpuinfo; cat /proc/meminfo; }
+# log() { eval "$*" >> menu.log; edit menu.log; }
 
 bar__() { for i in {1..26}; do printf "%s" $1; done; }
 printbar() { printf "$C_FRM<"; bar__ "-"; printf "|%14s%-14s|" $1 $2; bar__ "-"; printf ">%s$R" $'\n'; }
@@ -71,9 +74,9 @@ menu_main() {
 
 	BANNER=${BANNER:-figlet}
 	ascii_banner_installed=$(which $BANNER) # on cygwin, 'which' prints always the path
-	${reset:-clear}
+	eval "${before:-clear}"
 	echo -en $LYELLOW
-	if [ "$ascii_banner_installed" != "" ]; then $BANNER "$NAME"; else head -n 42 $0 | tail -n 6; fi
+	if [ "$ascii_banner_installed" != "" ]; then $BANNER "$NAME"; else head -n 41 $0 | tail -n 6; fi
 	printbar "PLEASE SELECT" " $NAME"
 	select i in $LIST ; do 
 		[ "$i" == "" ] || [ "$i" == "0" ] && break
@@ -82,9 +85,9 @@ menu_main() {
 		CMD=$(echo "$CMD" | sed 's/\x1b\[[0-9;]*m//g')
 		echo -en $C_FRM$CMD$R
 		[ "$CMD" != "" ] && declare -x $NAME="$CMD";
-		[ "$RUNLINE" == "true" ] && eval "$CMD"
+		[ "$RUNLINE" == "true" ] && eval "$CMD" || break
 	done
-	echo "$NAME=$CMD" >> menu-$USERNAME.def
+	echo "$NAME=\"$CMD\"" >> menu-$USERNAME.def
 	printbar "$NAME: " "$CMD"
 }
 
