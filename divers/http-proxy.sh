c1bb089440945d0d546ecc714c298c4cf1dcb6ee
Thomas Schneider <snieda@web.de>
2024-04-12 16:56:57 +0200

    http-proxy with loop

--- divers/http-proxy.sh
+++ divers/http-proxy.sh
@@ -2,23 +2,52 @@
 #-----------------------------------------------------------------------------
 # simple http proxy through netcat (Thomas Schneider 2020)
 #
-# usage: $0 <dest-host> [dest-port (default:80)] [src-port (default: 8888)] 
+# usage: $0 [proxy-port (default: 8888)] [sendsed=<my-sed-expr>] [recvsed=<<my-sed-expr>]
 #           [ncopt=<netcat options>]
 #-----------------------------------------------------------------------------
 
-[ $# == 0 ] || [ "$1" == "--help" ] && head -n 7 $0 && exit 1
+[ "$1" == "--help" ] && head -n 7 $0 && exit 1
 echo -en "setting parameters: "; for a in "$*" ; do [[ "$a" == *"="* ]] && declare -gt $a && echo $a && shift; done;
 echo
 
-DEST=$1 DEST_PORT=${2:-80} PORT=${3:-8888}
-TMP=`mktemp -d`
-BACK=$TMP/pipe.back
-SENT=$TMP/pipe.sent
-RCVD=$TMP/pipe.rcvd
-#trap 'rm -rf "$TMP"' EXIT
-mkfifo -m 0600 "$BACK" "$SENT" "$RCVD"
-sed 's/^/=> /' <"$SENT" &
-sed 's/^/  <= /' <"$RCVD" &
-echo "listening on $PORT"
-echo "use curl -x localhost:$PORT or set shell vars http(s)_port to use as proxy!"
-nc $ncopt -k -l -p "$PORT" <"$BACK" | tee "$SENT" | nc $ncopt "$DEST" "$DEST_PORT" | tee "$RCVD" >"$BACK"
+set -x
+nc="pwncat -v" # perhaps you want to use ncat or pwncat
+PORT=${1:-9999}
+sendsed=${sendsed:-s///}
+recvsed=${recvsed:-s///}
+rm fifo sent
+mkfifo fifo sent
+
+response_loop() {
+  echo "starting proxy loop"
+  while true; do
+    if read -r line <sent; then
+      echo "$line"
+      echo $line | $nc -v $(geturl $line) | tee fifo
+    fi
+  done
+}
+
+request_loop() {
+  echo "starting response loop"
+  while true; do
+    if read -r line <sent; then
+      echo "$line"
+      echo $line | $nc $ncopt -v -l $PORT < fifo | tee | tee sent &
+   fi
+  done
+}
+
+geturl() {
+    cut "$1" -d ' ' -f 2
+}
+
+echo "listening on $PORT    (use curl -x localhost:$PORT or set shell vars http(s)_port to use as proxy!)"
+#$nc $ncopt -k -l $PORT < fifo | tee | tee sent | sed -e $sendsed | call | tee | sed -e $recvsed > fifo
+
+# start the proxy
+#$nc $ncopt -v -k -l $PORT < fifo | tee | tee sent &
+
+request_loop &
+response_loop
+
