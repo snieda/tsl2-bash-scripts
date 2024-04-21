e9f4d98858592466522e784a741818f22423ffdb
Thomas Schneider <snieda@web.de>
2024-04-21 18:16:26 +0200

    NEW: diff-ignore.sh

---
+++ divers/diff-ignore.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# do a diff ignoring a given regular expression like a timestamp
+# usage: $0 {file1} {file2} [IGNORE=my-regular-expression-to-ignore]
+source main-args.sh
+FILE1=${FILE1:-$1}
+FILE2=${FILE2:-$2}
+IGNORE=${IGNORE:-'[0-9]{8}-[0-9]{6}'}
+FILTER="sed -r s/$IGNORE/XXX/p"
+DIFFER="icdiff -N"
+#DRYRUN=printf "%s %s %s "
+$DRYRUN $DIFFER <($FILTER $FILE1) <($FILTER $FILE2)
