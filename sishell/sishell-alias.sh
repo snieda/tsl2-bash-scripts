afefa8f88e32fa8b64bd823d5ec2ed3fbb4d803c
Thomas Schneider <snieda@web.de>
2025-01-18 18:49:00 +0100

    sishell extended

--- sishell/sishell-alias.sh
+++ sishell/sishell-alias.sh
@@ -1,33 +1,15 @@
 # convenience variables, aliases and methods for styling
-E0='\x1b[00;'
-E1='\x1b[01;'
-C0="$E0$Im"
-C1="$EE[01;$Im"
+echo "evaluating sishell aliases and methods..."
+E0="\x1b[00;"
+E1="\x1b[01;"
 I=30
 
-RESTORE='\x1b[0m'
-R=$RESTORE
-
 COLORS="RED GREEN YELLOW BLUE PURPLE CYAN LIGHTGRAY"
-for c in $COLORS; do I="$I+1"; declare $c="$C0"; echo "$c=$C0 "; done;
+for c in $COLORS; do I="$(($I+1))"; declare -x $c="$E0$I""m"; declare -x "L$c"="$E0$I""m"; echo "L$c=$E1$I""m"; done;
 
-RED='\x1b[00;31m'
-GREEN='\x1b[00;32m'
-YELLOW='\x1b[00;33m'
-BLUE='\x1b[00;34m'
-PURPLE='\x1b[00;35m'
-CYAN='\x1b[00;36m'
-LIGHTGRAY='\x1b[00;37m'
-
-LRED='\x1b[01;31m'
-LGREEN='\x1b[01;32m'
-LYELLOW='\x1b[01;33m'
-LBLUE='\x1b[01;34m'
-LPURPLE='\x1b[01;35m'
-LCYAN='\x1b[01;36m'
-WHITE='\x1b[01;37m'
-
-C_FRM=$LBLUE
-C_CMD=$LCYAN
-C_PAR=$LGREEN
-C_CMT=$LRED
+export C_FRM=$LBLUE
+export C_CMD=$LCYAN
+export C_PAR=$LGREEN
+export C_CMT=$LRED
+export RESTORE='\x1b[0m'
+export R=$RESTORE
